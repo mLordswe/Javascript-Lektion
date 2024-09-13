eb22b8dbe8bcdbdedd49796e1836bc494f11536c
mLordswe <alexander.csore@gmail.com>
2024-09-13 10:13:47 +0200

    spaghetticode

--- vecka37/onsdag/app.js
+++ vecka37/onsdag/app.js
@@ -12,6 +12,8 @@ få beställning att upprepas tills användaren går vidare till nästa steg
 
 
 */
+let order = [];
+let orderPrice = 0;
 
 const toppings = {
   feta: ["Feta-ost", 12],
@@ -19,11 +21,15 @@ const toppings = {
   Oliver: ["Oliver", 5],
 };
 
-const pizzaSize = {
-  small: 30,
-  medium: 60,
-  large: 90,
-};
+let pizzaprice = [(Small = 60), (medium = 90), (Large = 120)];
+let pizzaSize = [
+  (Small += "kr small"),
+  (medium += "kr medium"),
+  (Large += "kr Large"),
+];
+for (const element of pizzaSize) {
+  console.log(element);
+}
 
 const pizza = {
   kebab: "KebabPizza",
@@ -36,11 +42,6 @@ const pizzaContains = {
   margarita: ["tomat", "ost"],
 };
 
-userInput = prompt(
-  `Vilken pizza vill du ha?vi har\n ${[
-    [pizza.kebab, pizza.Vesuvio, pizza.margarita],
-  ]}`
-);
 
 const toppingsMenu = () => {
   let wantsTopping = confirm("Vill du ha topping?");
@@ -48,10 +49,16 @@ const toppingsMenu = () => {
     userInput = prompt(`Vilken topping vill du ha?`);
     switch (userInput) {
       case "oliver":
-        return console.log("Blä oliver");
+        console.log("Du lade till oliver på din beställning");
+        order.push(toppings.Oliver);
+        return;
       case "mozarella":
+        console.log("Du lade till mozarella på din beställning");
+        order.push(toppings.mozarella);
         return;
       case "Fetaost":
+        console.log("Du lade till fetaost på din beställning");
+        order.push(toppings.feta);
         return;
 
       default:
@@ -61,24 +68,41 @@ const toppingsMenu = () => {
 };
 
 const meny = () => {
+  let villbestalla = confirm("Vill du beställa en pizza?");
+  userInput = prompt(
+    `Vilken pizza vill du ha?vi har\n ${[
+      [pizza.kebab, pizza.Vesuvio, pizza.margarita],
+    ]}`
+  );
+
+  order.push(
+    prompt(`Vilken storlek vill du ha på pizzan? \n Vi har ${pizzaSize}`)
+  );
+  console.log("Ny beställning");
   switch (userInput) {
     case "kebabpizza":
       console.log("Du valde en KebabPizza");
+      order.push(pizza.kebab);
       toppingsMenu();
-      return;
+      return orderPrice + ;
 
-    case "Vesuvio":
+    case "vesuvio":
       toppingsMenu();
+      order.push(pizza.Vesuvio);
       console.log("Du valde Vesuvio");
       return;
 
-    case "Margarita":
+    case "margarita":
       console.log("Du valde en Margarita");
+      order.push(pizza.margarita);
       toppingsMenu();
       return;
 
     default:
+      console.log("ERROR");
       break;
   }
+  villbestalla = confirm("Vill du beställa mer?");
 };
 meny();
+console.log(`du beställde ${order} som kostar ${orderPrice}`);
